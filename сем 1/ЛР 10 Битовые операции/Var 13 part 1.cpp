76c526173e54ff7e545e850c8aded2e4ca5e6f0c
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-04-05 10:05:52 +0300

    Лабораторная работа № 10. Битовые операции
    1.Ввести целое число A. Извлечь 2 бита числа A, начиная с пятого и вставить их в число B, начиная также с пятого бита.
    Инвертировать в 1 в числе А n битов вправо от позиции p.

---
+++ сем 1/ЛР 10 Битовые операции/Var 13 part 1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+void main()
+{
+    setlocale(LC_CTYPE, "Russian");
+
+    cout << "Вариант 13\n\n";
+    cout << "Задание №1" << endl;
+
+
+	char tmp[33];
+	int A, B, maskA = 124;
+	int maskB = ~maskA >> 4;
+
+	cout << "Вариант 13\n\n";
+
+	cout << "Задание №1\n";
+	cout << "Первое число А="; cin >> A;
+	cout << "Второе число В="; cin >> B;
+
+	_itoa_s(A, tmp, 2);
+	cout << "A=" << tmp << endl;
+	_itoa_s(B, tmp, 2);
+	cout << "B=" << tmp << endl;
+
+	_itoa_s(maskA, tmp, 2);
+	cout << "Маска для А: " << tmp << endl;
+	_itoa_s((A & maskA) >> 2, tmp, 2);
+	cout << "Выделенные биты А: " << tmp << endl;
+
+	_itoa_s(maskB, tmp, 2);
+	cout << "Маска для В: " << tmp << endl;
+	_itoa_s(B & maskB, tmp, 2);
+	cout << " Очищены биты в B: " << tmp << endl;
+
+	_itoa_s(((B & maskB) | ((A & maskA) >> 1)), tmp, 2);
+	cout << " Результат B=" << tmp << endl << endl;
+
+}
---
+++ сем 1/ЛР 10 Битовые операции/Var 13 part 2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream> 
+using namespace std;
+
+void main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	cout << "Вариант 13\n\n";
+	cout << "Задание №1" << endl;
+
+	
+	char tmp[33], maskA[33];
+	int A, B, maskB, maskC, numb_of_bit, cicle_bit;;
+	cout << "Первое число A="; cin >> A;
+	_itoa_s(A, tmp, 2); cout << tmp << endl;
+	_itoa_s(A, maskA, 2); /*Запись числа A в маскуА в двоичном коде*/
+	cout << "Второе число B="; cin >> B;
+	_itoa_s(B, tmp, 2); cout << tmp << endl;
+	cout << "Введите с какого бита "; cin >> numb_of_bit;
+	numb_of_bit = numb_of_bit - 1;
+	cout << "Введите сколько бит меняем "; cin >> cicle_bit;
+	for (int i = 1; i <= cicle_bit; i++) {
+		A = A | (0001 << numb_of_bit); /*Замена определённого бита на 1*/
+		_itoa_s(A, tmp, 2); cout << tmp << endl;
+		numb_of_bit += 1;
+	}
+
+}
