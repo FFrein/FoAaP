9136c929e79ea7a6ebe7409dfcc4df44b9f29a61
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-04-05 09:58:25 +0300

    Лабораторная работа № 6. Отладка программ
    Реализовать формулу

---
+++ сем 1/ЛР 6 Отладка программ/Main_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main()
+{
+    using namespace std;
+    setlocale(LC_CTYPE, "Russian");
+    float f = -125e-6, y = 1.7, t = 1, c, d;
+    while (t <= 3)
+    {
+        t = t + 0.1;
+        c = 2 * sin(f / 2) + log(t);
+        if (c >= 3) {
+            d = y * exp(-2 * t) + f;
+            
+        }
+        else if (c < 3) {
+            d = y - pow(y, 3);
+            
+        }
+        cout << "t = " << t << endl;
+        cout << "d = " << d << endl;
+    }
+    return 0;
+}
