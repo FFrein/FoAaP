e704787e1191000da7036c4f124b6769b053ccd9
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-04-05 10:03:35 +0300

    Лабораторная работа № 9. Одномерные массивы
    Вар 12
    Найти минимальный элемент массива Т и заменить им четные по номеру элементы.
    вар 14
    В массиве несколько нулевых элементов. Найти первый и последний нулевые элементы. Вывести их индексы.

---
+++ сем 1/ЛР 9 Одномерные массивы/OAIP Laba 9 main 1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <ctime>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	const int N = 100;
+	int i, size, Massive[N];
+	int rmn = 0, rmx = 99, T = 0;
+	cout << "Введите размер массива от " << rmn << " до " << rmx << endl;
+	cin >> size;
+	if (size > rmx) {
+		return 0;
+	}
+	if (size < rmn) {
+		return 0; }
+	/*присваивание массиву чисела*/
+	srand((unsigned)time(NULL));
+	for (i = 0; i < size; i++)
+	{
+		Massive[i] = (int)(((double)rand() /
+			(double)RAND_MAX) * (rmx - rmn) + rmn);
+	}
+	/*find min*/
+	for (i = 0; i < size; i++) {
+		if (Massive[i] < Massive[T]) {
+			T = i;
+		}
+	}
+	cout << "Min = " << T << endl;
+	/*Проверка деления на 2 и замена*/
+	for (i = 0; i < size; i++) {
+		if (Massive[i] % 2 == 0) {
+			Massive[i] = T;
+			cout << Massive[i] << endl;
+		}
+		else {
+			cout << Massive[i] << endl;
+		}
+	}
+	return 0;
+}
---
+++ сем 1/ЛР 9 Одномерные массивы/OAIP Laba 9 main 2.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <ctime>
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	const int N = 100;
+	int i, size, Massive[N];
+	int rmn = 0, rmx = 99, T = 0, first, last;
+	cout << "Введите размер массива от " << rmn << " до " << rmx << endl;
+	cin >> size;
+	if (size > rmx) {
+		return 0;
+	}
+	if (size < rmn) {
+		return 0;
+	}
+	/*присваивание массиву чисела*/
+	srand((unsigned)time(NULL));
+	for (i = 0; i < size; i++)
+	{
+		Massive[i] = (int)(((double)rand() / (double)RAND_MAX) * (rmx - rmn) + rmn);
+	}
+	/*find min*/
+	for (i = 0; i < size; i++) {
+		if (Massive[i] == 0) {
+			if (T == 0) {
+				first = i;
+				T = 1;
+			}
+			last = i;
+		}
+	}
+	if (T == 1) {
+		cout << "Первый = " << first << " Последний = " << last;
+	}
+	else {
+		cout << "Нету";
+	}
+	return 0;
+}
