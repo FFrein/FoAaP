b048e08ecd66c925fc6ef03b3773c34103d3b436
Песецкий Никита Андреевич <pesetsky2003g@gmail.com>
2022-04-05 10:01:35 +0300

    Лабораторная работа № 7. Вычисление сумм, произведений, экстремумов
    В соответствии со своим вариантом разработать программу по условию, приведенному в таблице ниже. Произвести отладку программы.

---
+++ сем 1/ЛР 7 Вычисление сумм, произведений, экстремумов/main 1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+int main()
+{
+	using namespace std;
+	setlocale(LC_CTYPE, "Russian");
+	float z, x, xtwo = 0, xsum = 0, y, i = 0;
+	do {
+		i = i + 1;
+		cout << "Введите x" << i << "= ";
+		cin >> x;
+		xsum += x;
+		if (x > xtwo) {
+			xtwo = x;
+			cout << "Максимальное число обновлено" << endl;
+		}
+		
+	} while(i != 5);
+	z = xtwo;
+	y = z * xsum;
+	cout << "y= " << y;
+	return 0;
+}
